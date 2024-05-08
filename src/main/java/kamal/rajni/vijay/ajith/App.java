225297bfd7b33b8f2c66aa0db6c0c9780f62eaa5
Bala <Daniel123@gmail.com>
2024-05-08 12:32:15 +0530

    Done Doiiii

---
+++ src/main/java/kamal/rajni/vijay/ajith/App.java
@@ -0,0 +1,13 @@
+package kamal.rajni.vijay.ajith;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
